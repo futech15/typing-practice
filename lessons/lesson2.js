f2d16216aa1a9402d56de82fcac9ae9232f46319
Rabar Sami <rabar.sami.ali@gmail.com>
2026-09-15 12:44:22 -0400

    Create lesson2.js

---
+++ lessons/lesson2.js
@@ -0,0 +1,6 @@
+const lesson2 = {
+  id: 2,
+  title: "Lesson 2: D, F, J, and K",
+  description: "Practice moving your fingers between the home row and the row below.",
+  text: "dfjk kjfd fdjk jkfd dkjf fjkd kfdj jdfk fddj kkff jdkf fk dj dkjk fjfj dkfd kfjk"
+};
