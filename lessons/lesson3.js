2ca41805e3ba8d51e512b8750d49ae8598f55d52
Rabar Sami <rabar.sami.ali@gmail.com>
2026-09-15 12:44:38 -0400

    Create lesson3.js

---
+++ lessons/lesson3.js
@@ -0,0 +1,6 @@
+const lesson3 = {
+  id: 3,
+  title: "Lesson 3: R, T, Y, and U",
+  description: "Practice reaching upward from the home row using R, T, Y, and U.",
+  text: "rtyu uytr rytu yurt truy urty tyur ryut tuyr rruu ttyy yyrr uutt ryry tyty urur trtr"
+};
