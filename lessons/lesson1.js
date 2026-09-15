abca55360628ec06619c3f3150b5efb43f658870
Rabar Sami <rabar.sami.ali@gmail.com>
2026-09-15 15:28:21 -0400

    Update lesson1.js

--- lessons/lesson1.js
+++ lessons/lesson1.js
@@ -1,7 +1,6 @@
-window.lessonsList = window.lessonsList || [];
-window.lessonsList.push({
+var lesson1 = {
   id: 1,
   title: "Lesson 1: F, G, H, and J",
   description: "Practice using the four home-row keys: F, G, H, and J.",
   text: "fghj jhgf fjgh ghjf hfgj jfg hgjf gfhh jjgg ffhh gjfj hgjg fjhf ghgj jhfg fgjh hgjf"
-});
+};
