5477f0b5b6cbf4c50a382ef931010dc568242901
Rabar Sami <rabar.sami.ali@gmail.com>
2026-09-15 12:44:57 -0400

    Create lesson4.js

---
+++ lessons/lesson4.js
@@ -0,0 +1,6 @@
+const lesson4 = {
+  id: 4,
+  title: "Lesson 4: Mixed Finger Practice",
+  description: "Practice combining home-row and upper-row keys with different finger movements.",
+  text: "fr hj dk ty gf ju rd kh ft yj gr fu hd kt jf ry dh gu fj tk yh dr kg fu jt hr yd fk tu gj rh kd yf"
+};
