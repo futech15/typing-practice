493c39e57b00bc30ca8b0557058cddad3bb4d2dd
Rabar Sami <rabar.sami.ali@gmail.com>
2026-09-15 15:28:55 -0400

    Update lesson2.js

--- lessons/lesson2.js
+++ lessons/lesson2.js
@@ -1,5 +1,4 @@
-window.lessonsList = window.lessonsList || [];
-window.lessonsList.push({
+var lesson2 = {
   id: 2,
   title: "Lesson 2: D, K, S, and L",
   description: "Expand your home-row reach with D, K, S, and L.",
