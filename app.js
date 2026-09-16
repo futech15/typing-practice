54ac0425856ea5759e08c15e1daf76c3df6b3c7e
Rabar Sami <rabar.sami.ali@gmail.com>
2026-09-16 09:31:08 -0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const rawLessons = [
   typeof lesson1 !== "undefined" ? lesson1 : null,
   typeof lesson2 !== "undefined" ? lesson2 : null,
   typeof lesson3 !== "undefined" ? lesson3 : null,
-  typeof lesson4 !== "undefined" ? lesson4 : null
+  typeof lesson4 !== "undefined" ? lesson4 : null,
+  typeof lesson5 !== "undefined" ? lesson5 : null
 ];
 
 const lessons = rawLessons.filter(Boolean);
