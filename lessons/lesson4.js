419e005587a58adc9de7eed23292a9906820e73e
Rabar Sami <rabar.sami.ali@gmail.com>
2026-09-16 08:47:04 -0400

    Update lesson4.js

--- lessons/lesson4.js
+++ lessons/lesson4.js
@@ -3,5 +3,4 @@ var lesson4 = {
   title: "Lesson 4: Full Home Row",
   description: "Combine all home-row keys together.",
   text: "fr hj dk ty gf ju rd kh ft yj gr fu hd kt jf ry dh gu fj tk yh dr kg fu jt hr yd fk tu gj rh kd yf"
-});
- 
+};
