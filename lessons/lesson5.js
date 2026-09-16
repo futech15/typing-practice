76042aeb336e85290b5414b7b190c40cd961e187
Rabar Sami <rabar.sami.ali@gmail.com>
2026-09-16 09:29:22 -0400

    Create lesson5.js

---
+++ lessons/lesson5.js
@@ -0,0 +1,6 @@
+var lesson5 = {
+  id: 5,
+  title: "Lesson 5",
+  description: "Practice typing a funny story using letters, words, and punctuation.",
+  text: "on friday, a small robot named max went to school; max wanted to learn how to type; he typed so fast that his keyboard began to shake; the teacher said, slow down, max; max smiled and typed, i can do it; then the robot typed a funny message for the whole class; everyone laughed and gave max a high five"
+};
