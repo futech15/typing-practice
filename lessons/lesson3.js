5a94e3b7faa133840e1cc46246e0515509518161
Rabar Sami <rabar.sami.ali@gmail.com>
2026-09-16 08:46:49 -0400

    Update lesson3.js

--- lessons/lesson3.js
+++ lessons/lesson3.js
@@ -1,6 +1,6 @@
 var lesson3 = {
   id: 3,
-  title: "Lesson 3: A and Semi-colon (;)",
-  description: "Master the outer home-row keys: A and ;.",
+  title: "Lesson 3: R, T, Y, and U",
+  description: "Practice reaching the upper-row keys R, T, Y, and U.",
   text: "rtyu uytr rytu yurt truy urty tyur ryut tuyr rruu ttyy yyrr uutt ryry tyty urur trtr"
-});
+};
