281599fe4152c422152542a8937c6b4bc65a4051
Rabar Sami <rabar.sami.ali@gmail.com>
2026-09-17 10:49:27 -0400

    Create lesson6.js

---
+++ lessons/lesson6.js
@@ -0,0 +1,6 @@
+var lesson6 = {
+  id: 6,
+  title: "Lesson 6: J Key Control",
+  description: "Hold the J key while typing the lesson text.",
+  text: "we are ready\nwe see a great day\nthe red fox ran fast\nfred had a safe start\nhe was glad to see the sun\nthe team had a great day"
+};
