f40dd14ffab458c917ca957d83bcee03f78dbc7b
Rabar Sami <rabar.sami.ali@gmail.com>
2026-09-17 16:02:43 -0400

    Update lesson6.js

--- lessons/lesson6.js
+++ lessons/lesson6.js
@@ -2,5 +2,7 @@ var lesson6 = {
   id: 6,
   title: "Lesson 6",
   description: "Hold the J key with your right hand while typing with your left hand.",
+  requiredHoldKey: "j",
+  requiredHand: "right",
   text: "we see a red cat. fred had a safe start. a fast car fled. we ate a sweet treat. red star falls."
 };
