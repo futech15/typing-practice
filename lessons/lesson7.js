88eedba7f58a1132cd7318ff14f2f2b4b8e7434c
Rabar Sami <rabar.sami.ali@gmail.com>
2026-09-18 08:18:24 -0400

    Update lesson7.js

--- lessons/lesson7.js
+++ lessons/lesson7.js
@@ -2,5 +2,5 @@ var lesson7 = {
   id: 7,
   title: "Lesson 7: Funny Words",
   description: "Practice typing short and funny sentences.",
-  text: "the fat cat sat on a hat\nfred ate a big red egg\nsam ran fast and fell\nwe had fun at the farm"
+  text: "The fat cat sat on a hat. Fred ate a big red egg\nSam ran fast and fell. We had fun at the farm"
 };
