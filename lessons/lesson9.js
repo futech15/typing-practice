2c2b07b06af297a8631dbc877df41989daa0941c
Rabar Sami <rabar.sami.ali@gmail.com>
2026-09-18 08:03:51 -0400

    Create lesson9.js

---
+++ lessons/lesson9.js
@@ -0,0 +1,6 @@
+var lesson9 = {
+  id: 9,
+  title: "Lesson 9: Funny Day",
+  description: "Keep typing while practicing short funny sentences.",
+  text: "fred had a big mess\nhis cat ate the ham\nsam said that was bad\nwe all had a good laugh"
+};
