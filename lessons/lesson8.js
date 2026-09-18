5bd5b12f25ce513e2e73eb26b8b85d0c8570ddda
Rabar Sami <rabar.sami.ali@gmail.com>
2026-09-18 08:02:54 -0400

    Create lesson8.js

---
+++ lessons/lesson8.js
@@ -0,0 +1,6 @@
+var lesson8 = {
+  id: 8,
+  title: "Lesson 8: Silly Sentences",
+  description: "Type these silly sentences and keep your fingers moving.",
+  text: "the dog ate my snack\nmax had a red hat\nsam saw a fat rat\nthe cat ran very fast"
+};
