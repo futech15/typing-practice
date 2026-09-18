571df6f869fa358928107fd3e6ef6f37bb745994
Rabar Sami <rabar.sami.ali@gmail.com>
2026-09-18 08:04:27 -0400

    Create lesson10.js

---
+++ lessons/lesson10.js
@@ -0,0 +1,6 @@
+var lesson10 = {
+  id: 10,
+  title: "Lesson 10: HSA Middle School Life",
+  description: "A funny little story about a day at Horizon Science Academy.",
+  text: "sam was late for class\nfred forgot his homework\nmax ran fast to lunch\nthen the bell rang again\nwe all laughed and got back to class"
+};
