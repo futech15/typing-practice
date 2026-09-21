f186b840af6c96f211494b5254d425e88052c319
Rabar Sami <rabar.sami.ali@gmail.com>
2026-09-21 10:04:48 -0400

    Create lesson12.js

---
+++ lessons/lesson12.js
@@ -0,0 +1,6 @@
+var lesson12 = {
+  id: 12,
+  title: "Lesson 12: School Life Today",
+  description: "Practice typing about everyday school life.",
+  text: "the school day starts early and students get ready for class\nwe learn new things and work together every day"
+};
