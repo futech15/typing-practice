70c93a485314199b4b1152605da4390540fd762d
Rabar Sami <rabar.sami.ali@gmail.com>
2026-09-21 10:05:24 -0400

    Create lesson13.js

---
+++ lessons/lesson13.js
@@ -0,0 +1,6 @@
+var lesson13 = {
+  id: 13,
+  title: "Lesson 13: Did You Know?",
+  description: "Practice typing a fact about our school.",
+  text: "did you know Horizon Science Academy Cleveland Middle School has a four star **** overall rating\nwe are proud of our school"
+};
