c95b8daab3cb988114aa22515469df3329f417e1
Rabar Sami <rabar.sami.ali@gmail.com>
2026-09-21 10:06:20 -0400

    Create lesson15.js

---
+++ lessons/lesson15.js
@@ -0,0 +1,6 @@
+var lesson15 = {
+  id: 15,
+  title: "Lesson 15: New School Year",
+  description: "Practice typing about the new school year.",
+  text: "a new school year brings new classes, new friends, and new challenges\nthere is always something new to learn"
+};
