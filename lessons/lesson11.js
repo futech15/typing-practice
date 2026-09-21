db4affcc55de9fc1b260e3f935e82816808d7c30
Rabar Sami <rabar.sami.ali@gmail.com>
2026-09-21 08:52:00 -0400

    Create lesson11.js

---
+++ lessons/lesson11.js
@@ -0,0 +1,7 @@
+const lesson11 = {
+  id: "lesson-11",
+  title: "Lesson 11: Boss Battle",
+  description: "Type accurately to cast spells and defeat the troll!",
+  text: "quick wizard spells overcome tough battle obstacles",
+  isBattleLesson: true
+};
