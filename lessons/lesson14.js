7890784337d4fb0e9aa87f3e30e07f101aa857f8
Rabar Sami <rabar.sami.ali@gmail.com>
2026-09-21 10:06:01 -0400

    Create lesson14.js

---
+++ lessons/lesson14.js
@@ -0,0 +1,6 @@
+var lesson14 = {
+  id: 14,
+  title: "Lesson 14: CYSP Kickoff",
+  description: "Practice typing about CYSP.",
+  text: "last week we had our CYSP kickoff dinner, did you participate?\nare you part of CYSP?"
+};
